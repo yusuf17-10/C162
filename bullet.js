bac0a0d7f1a6292d26ddf3f5b8c0eb16c4a809a0
yusuf17-10 <64640684+yusuf17-10@users.noreply.github.com>
2021-10-29 21:17:24 +0530

    Add files via upload

---
+++ bullet.js
@@ -0,0 +1,34 @@
+
+AFRAME.registerComponent('bullets', {
+    schema: {
+
+    },
+
+    init: function () {
+        this.shoot_bullet()
+    },
+
+    shoot_bullet: function () {
+        window.addEventListener("keydown", (e) => {
+            if (e.key == "z") {
+                var bullet = document.createElement("a-entity")
+                bullet.setAttribute("geometry", { primitive: "sphere", radius: 50 })
+                console.log("hello")
+                bullet.setAttribute("material", "color", "black")
+                var cam = document.querySelector("#camera")
+                pos = cam.getAttribute("position")
+                bullet.setAttribute("position", {
+                    x: pos.x, y: pos.y, z: pos.z
+                })
+                var cam2 = document.querySelector("#camera").object3D
+                var direction = new THREE.Vector3()
+                cam2.getWorldDirection(direction)
+                bullet.setAttribute("velocity",direction)
+                var scene = document.querySelector("#scene")
+                scene.appendChild(bullet)
+            }
+
+        })
+    },
+
+});
